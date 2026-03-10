498f25ea4de3a97122e7319453ca57322fec3a18
Pablo-Data-Analytics <119531241+PabloHenrique22@users.noreply.github.com>
2026-03-10 14:25:59 -0300

    Update analysis.sql

--- dataset/queries/analysis.sql
+++ dataset/queries/analysis.sql
@@ -1,1 +1,17 @@
+-- Receita total por cidade
 
+SELECT
+    city,
+    SUM(total) AS total_sales
+FROM supermarket_sales
+GROUP BY city
+ORDER BY total_sales DESC;
+
+-- Método de pagamento mais utilizado
+
+SELECT
+    payment,
+    COUNT(*) AS total_transactions
+FROM supermarket_sales
+GROUP BY payment
+ORDER BY total_transactions DESC;
