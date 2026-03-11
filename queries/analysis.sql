1a1ad3192b9ce1280edbf99afb55e50e7c57bc17
Pablo-Data-Analytics <119531241+PabloHenrique22@users.noreply.github.com>
2026-03-11 11:41:07 -0300

    Update analysis.sql

--- queries/analysis.sql
+++ queries/analysis.sql
@@ -1,3 +1,9 @@
+-- Faturamento Geral
+
+SELECT
+SUM(total) AS total_revenue
+FROM supermarket_sales;
+
 -- Receita total por cidade
 
 SELECT
@@ -7,6 +13,24 @@ FROM supermarket_sales
 GROUP BY city
 ORDER BY total_sales DESC;
 
+-- Receita por linha de produto
+
+SELECT
+product_line,
+SUM(total) AS revenue
+FROM supermarket_sales
+GROUP BY product_line
+ORDER BY revenue DESC;
+
+-- Quantidade de produtos vendidos
+
+SELECT
+product_line,
+SUM(quantity) AS items_sold
+FROM supermarket_sales
+GROUP BY product_line
+ORDER BY items_sold DESC;
+
 -- Método de pagamento mais utilizado
 
 SELECT
@@ -15,3 +39,45 @@ SELECT
 FROM supermarket_sales
 GROUP BY payment
 ORDER BY total_transactions DESC;
+
+-- Ticket médio
+
+SELECT
+ROUND(AVG(total),2) AS average_ticket
+FROM supermarket_sales;
+
+-- Ticket médio por cidade
+
+SELECT
+city,
+ROUND(AVG(total),2) AS average_ticket
+FROM supermarket_sales
+GROUP BY city
+ORDER BY average_ticket DESC;
+
+-- Receita por gênero
+
+SELECT
+gender,
+SUM(total) AS revenue
+FROM supermarket_sales
+GROUP BY gender;
+
+-- Ranking de cidades (Window Function)
+
+SELECT
+city,
+SUM(total) AS revenue,
+RANK() OVER (ORDER BY SUM(total) DESC) AS ranking
+FROM supermarket_sales
+GROUP BY city;
+
+-- Produtos mais vendidos
+
+SELECT
+product_line,
+SUM(quantity) AS total_items
+FROM supermarket_sales
+GROUP BY product_line
+ORDER BY total_items DESC;
+
